b0d5937ec054968515a5b69072c9de3e81743261
AiviriX <claveriakyle@gmail.com>
2021-08-22 12:58:04 +0800

    Base Files for Hovercraft AI

---
+++ src/VanillaExpanded/Content/VEXUnits/Ai/HovercraftAI.java
@@ -0,0 +1,15 @@
+package VanillaExpanded.Content.VEXUnits.Ai;
+
+import mindustry.ai.types.*;
+import arc.math.*;
+import mindustry.entities.units.*;
+import mindustry.gen.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+
+public class HovercraftAI extends AIController {
+    
+
+
+}
---
+++ src/VanillaExpanded/Content/VEXUnits/HovercraftUnitType.java
@@ -0,0 +1,5 @@
+package VanillaExpanded.Content.VEXUnits;
+
+public class HovercraftUnitType {
+    
+}
